2bea78b3fe1e851c6fd41b2393f7280616b23991
Jeongdonghun <arrrrr180@gmail.com>
2022-10-24 17:15:41 +0900

    S3 TEST

---
+++ src/main/java/com/mini/team3/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.mini.team3.controller;
+
+import com.mini.team3.s3.S3Uploader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    private final S3Uploader s3Uploader;
+
+    @PostMapping("/test")
+    public String test (@RequestPart(value = "img") MultipartFile multipartFile) {
+        try {
+            s3Uploader.uploadFiles(multipartFile, "testdir1");
+        } catch (Exception e){
+            return "실패";
+        }
+
+        return "성공";
+    }
+
+
+}
